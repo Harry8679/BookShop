8ae7dde4efd7678fd419325ecb9854bde409ac81
Harry7986 <emarh.harry.code@gmail.com>
2025-10-25 22:57:55 +0200

    Tailwind 5

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
-import symfonyPlugin from 'vite-plugin-symfony'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    symfonyPlugin(),
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
   build: {
-    outDir: 'public/build', // dossier de sortie pour les assets
+    outDir: 'public/build',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './assets/app.js', // Point d’entrée
+    },
   },
 })
