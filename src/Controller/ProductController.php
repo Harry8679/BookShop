e5a01f95efb0835ec81dcddd6fea6cecebf908d2
Harry7986 <emarh.harry.code@gmail.com>
2025-11-08 19:17:10 +0100

    Get All Products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,21 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/boutique', name: 'app_shop_index')]
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findBy(
+            ['isPublished' => true],
+            ['createdAt' => 'DESC']
+        );
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
         ]);
     }
 
-    #[Route('/product/{id}', name: 'app_product_show')]
+    #[Route('/produit/{id}', name: 'app_product_show')]
     public function show(Product $product): Response
     {
         return $this->render('product/show.html.twig', [
