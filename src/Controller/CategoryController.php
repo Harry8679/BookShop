b5dec1793d6d37a5abc4b59727e5881dbd882717
Harry7986 <emarh.harry.code@gmail.com>
2025-11-08 13:14:12 +0100

    Category Controller 2

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class CategoryController extends AbstractController
+class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    #[Route('/category/{id}', name: 'app_category_show')]
+    public function show(Category $category): Response
     {
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+        // On récupère les produits liés à cette catégorie
+        $products = $category->getProducts();
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'products' => $products,
         ]);
     }
 }
