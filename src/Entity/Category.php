9a99e75959f78c4b319a69f83da43c5276da69d2
Harry7986 <emarh.harry.code@gmail.com>
2025-11-08 11:50:00 +0100

    Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,8 +6,11 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[Vich\Uploadable]
 class Category
 {
     #[ORM\Id]
@@ -24,6 +27,16 @@ class Category
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class)]
     private Collection $products;
 
+    // 🖼️ Image uploadée
+    #[Vich\UploadableField(mapping: 'categories_images', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private ?\DateTimeInterface $updatedAt = null;
+
     public function __construct() { $this->products = new ArrayCollection(); }
 
     public function getId(): ?int { return $this->id; }
@@ -36,4 +49,18 @@ class Category
     public function getProducts(): Collection { return $this->products; }
 
     public function __toString(): string { return $this->name; }
+
+    // --- Image ---
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+        if ($imageFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File { return $this->imageFile; }
+    public function getImageName(): ?string { return $this->imageName; }
+    public function setImageName(?string $imageName): void { $this->imageName = $imageName; }
+    public function getUpdatedAt(): ?\DateTimeInterface { return $this->updatedAt; }
 }
