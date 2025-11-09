59ab78b4cd65d3b084817c0b9f1071180363f538
Harry7986 <emarh.harry.code@gmail.com>
2025-11-09 07:21:53 +0100

    User Password 5

--- src/Form/UserPasswordType.php
+++ src/Form/UserPasswordType.php
@@ -2,30 +2,42 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('phone')
-            ->add('isVerified')
-        ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
+            ->add('oldPassword', PasswordType::class, [
+                'label' => 'Mot de passe actuel',
+                'mapped' => false, // 💡 Important : n’est pas lié à l’entité
+                'attr' => ['placeholder' => 'Entrez votre mot de passe actuel', 'class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer votre mot de passe actuel.'])
+                ],
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false, // 💡 aussi non lié à l’entité
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                    'attr' => ['placeholder' => 'Entrez un nouveau mot de passe', 'class' => 'form-control'],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le nouveau mot de passe',
+                    'attr' => ['placeholder' => 'Confirmez le mot de passe', 'class' => 'form-control'],
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez saisir un mot de passe.']),
+                    new Length(['min' => 6, 'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères.']),
+                ],
+            ]);
     }
 }
