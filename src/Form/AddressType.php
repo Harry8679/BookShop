100ac410ddcb1cc5ad9b5bab0bef3471f144c1e0
Harry7986 <emarh.harry.code@gmail.com>
2025-11-09 07:59:31 +0100

    Address Type

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Address;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,23 +15,25 @@ class AddressType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('street')
-            ->add('postalCode')
-            ->add('city')
-            ->add('country')
-            ->add('isBilling')
-            ->add('isShipping')
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('street', TextType::class, ['label' => 'Rue'])
+            ->add('postalCode', TextType::class, ['label' => 'Code postal'])
+            ->add('city', TextType::class, ['label' => 'Ville'])
+            ->add('country', CountryType::class, [
+                'label' => 'Pays',
+                'preferred_choices' => ['FR'],
             ])
-        ;
+            ->add('isBilling', CheckboxType::class, [
+                'label' => 'Adresse de facturation',
+                'required' => false,
+            ])
+            ->add('isShipping', CheckboxType::class, [
+                'label' => 'Adresse de livraison',
+                'required' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => Address::class,
-        ]);
+        $resolver->setDefaults(['data_class' => Address::class]);
     }
 }
