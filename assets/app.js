35fda62c25d3bef93dd2c6bf1fc381bbfb0502f9
Harry7986 <emarh.harry.code@gmail.com>
2025-11-09 04:41:48 +0100

    app.js

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,10 @@
-import './styles/app.css'
-import "@fortawesome/fontawesome-free/css/all.min.css";
+// === FICHIER: assets/app.js ===
+
+// Styles Tailwind
+import './styles/app.css';
+
+// Font Awesome (inclut toutes les icônes)
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// Si tu veux aussi les JS d’icônes dynamiques (facultatif)
+// import '@fortawesome/fontawesome-free/js/all.js';
