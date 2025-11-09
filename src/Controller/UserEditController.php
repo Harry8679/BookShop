3fa73f2bb6b37db7a359df58bf5d6bfdc265049b
Harry7986 <emarh.harry.code@gmail.com>
2025-11-09 07:04:10 +0100

    Edit Controller 2

--- src/Controller/UserEditController.php
+++ src/Controller/UserEditController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserEditType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-final class UserEditController extends AbstractController
+#[IsGranted('ROLE_USER')]
+class UserEditController extends AbstractController
 {
-    #[Route('/user/edit', name: 'app_user_edit')]
-    public function index(): Response
+    #[Route('/profil/modifier', name: 'app_user_edit')]
+    public function edit(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('user_edit/index.html.twig', [
-            'controller_name' => 'UserEditController',
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour modifier votre profil.');
+        }
+
+        $form = $this->createForm(UserEditType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', '✅ Profil mis à jour avec succès.');
+            return $this->redirectToRoute('app_user_profile');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'form' => $form,
         ]);
     }
 }
